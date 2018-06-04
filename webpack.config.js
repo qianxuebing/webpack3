ab70851524a77e7c3cf47a8f0ea6247676ed1d76
qianxuebing12 <qianxuebing92@163.com>
2018-06-04 14:24:07 +0800

    l

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require("path");
+// 引入html-webpack-plugin模块
 const htmlWebpackPlugin = require("html-webpack-plugin");
+// 引入clean-webpack-plugin模块
 const cleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname,"./src/js/app.js"),
+    // 入口
+    entry: path.resolve(__dirname,"./src/js/app.js"), //制定webpack打包的入口为app.js （配置路径用path.resolve()处理 ）
+    // 出口
     output:{
-         path: path.resolve(__dirname,"./dist"),
-        filename: "js/bundle-[hash:1].js"
+         path: path.resolve(__dirname,"./dist"), //指定打包后js文件放置的位置
+        filename: "js/bundle-[hash:1].js" //指定打包后的js名称，也是index.html最终引入的js名称
     },
+    // 初始化插件
     plugins:[
         new htmlWebpackPlugin({
             template:"index.html"
