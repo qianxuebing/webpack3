fe6b5c1934887b97513aa01daee9a5ee9d274578
qianxuebing12 <qianxuebing92@163.com>
2018-06-04 14:00:40 +0800

    chu

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const cleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry: path.resolve(__dirname,"./src/js/app.js"),
     output:{
          path: path.resolve(__dirname,"./dist"),
-        filename: "js/bundle.js"
-    }
+        filename: "js/bundle-[hash:1].js"
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template:"index.html"
+        }),
+        new cleanWebpackPlugin(["dist"],{
+            root:__dirname,
+            verbose:true,
+            dry:false
+        })
+    ]
 }
