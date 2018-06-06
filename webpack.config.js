5112125b74f89ea0df2f5bfd76e0f3ade3fc3990
qianxuebing12 <qianxuebing92@163.com>
2018-06-06 11:00:08 +0800

    over

--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,19 @@ module.exports = {
     // 出口
     output:{
          path: path.resolve(__dirname,"./dist"), //指定打包后js文件放置的位置
-        filename: "js/bundle-[hash:1].js" //指定打包后的js名称，也是index.html最终引入的js名称
+        filename: "js/bundle-[hash:1].js" //指定打包后的js名称，也是index.html最终引入的js名称,(hash：用于区别版本)
     },
     // 初始化插件
     plugins:[
+        // 根据模块动态生成html
         new htmlWebpackPlugin({
             template:"index.html"
         }),
+        // 删除dist文件夹下重复的文件
         new cleanWebpackPlugin(["dist"],{
-            root:__dirname,
-            verbose:true,
-            dry:false
+            root: __dirname, //指定插件根目录位置
+            verbose: true, //开启在控制台输出信息
+            dry: false ///启用删除文件
         })
     ]
 }
